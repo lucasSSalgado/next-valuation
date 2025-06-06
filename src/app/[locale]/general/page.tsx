f9d1297e48aa973a5423a632fb20b060bde07fca
lucasSSalgado <71613641+lucasSSalgado@users.noreply.github.com>
2025-06-06 17:28:29 -0300

    correcting build

--- src/app/[locale]/general/page.tsx
+++ src/app/[locale]/general/page.tsx
@@ -1,5 +1,7 @@
+import { unstable_setRequestLocale } from 'next-intl/server';
 import { redirect } from 'next/navigation';
 
 export default function GeneralPage({params: {locale}} : {params: {locale: string}}) {
+    unstable_setRequestLocale(locale);
     redirect(`/${locale}/general/compound-interest`);
 }
